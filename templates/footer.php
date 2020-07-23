6878ee6d907d6b0171d0e542ffcd24080ab08435
Akshat Gupta <akshatvg23@gmail.com>
2020-07-23 22:06:28 +0530

    libs

--- templates/footer.php
+++ templates/footer.php
@@ -8,6 +8,6 @@
     <script src=" https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" crossorigin="anonymous"></script>
-    <script src="assets/js/index.js"></script>
+    <script src="libs/js/index.js"></script>
 
     </body>
