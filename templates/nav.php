125f0b0265e9deee130bc8030ccf26d7169fb7df
Akshat Gupta <akshatvg23@gmail.com>
2020-07-26 01:32:52 +0530

    home link

--- templates/nav.php
+++ templates/nav.php
@@ -1,16 +1,16 @@
 <nav>
     <div class="nav-wrapper">
         <a href="#" data-target="sidenav-1" class="left sidenav-trigger show-on-large"><i class="material-icons white-text">menu</i></a>
-        <a href="/" class="brand-logo heading center" style="font-size: 23px;">EXC-VTOP</a>
+        <a href="index.php" class="brand-logo heading center" style="font-size: 23px;">EXC-VTOP</a>
     </div>
 </nav>
 
 <!-- Left Sidebar -->
 <ul id="sidenav-1" class="sidenav">
-    <a href="/" class="center-align">
+    <a href="index.php" class="center-align">
         <li class="mt-5"><img alt="EXC-VTOP" src="uploads/img/logo.png" class="responsive-img"></li>
     </a>
-    <li><a href="/" class="waves-effect grey lighten-4 center-align" id="active">Dashboard</a></li>
+    <li><a href="index.php" class="waves-effect grey lighten-4 center-align" id="active">Dashboard</a></li>
     <li class="mt-3"><a href="view2CC.php" class="waves-effect grey lighten-4 center-align">View 2CC Courses</a></li>
     <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">Add Achievements</a></li>
     <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">View Achievements</a></li>
