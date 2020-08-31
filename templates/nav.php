61f914af76d7619cba8fb812ea5c427a47a93ff6
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-08-31 11:20:09 +0530

    extra options removed

--- templates/nav.php
+++ templates/nav.php
@@ -10,11 +10,9 @@
     <a href="index.php" class="center-align">
         <li class="mt-5"><img alt="EXC-VTOP" src="uploads/img/logo.png" class="responsive-img"></li>
     </a>
-    <li><a href="index.php" class="waves-effect grey lighten-4 center-align" id="active">Dashboard</a></li>
+    <li><a href="index.php" class="waves-effect grey lighten-4 center-align">Dashboard</a></li>
     <li class="mt-3"><a href="view2CC.php" class="waves-effect grey lighten-4 center-align">View 2CC Courses</a></li>
-    <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">Add Achievements</a></li>
-    <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">View Achievements</a></li>
-    <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">Submit to Faculty</a></li>
+    <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">Achievements</a></li>
     <li class="mt-3"><a href="#!" class="waves-effect grey lighten-4 center-align">Faculty Info</a></li>
     <li class="mt-5 mb-5"><a href="/logout" class="waves-effect grey lighten-2 center-align"><i class="material-icons">exit_to_app</i>Logout</a></li>
 </ul>
